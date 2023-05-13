929a5b7a53a0186140a0ec2c291b879f345313a8
kang <1037650891@qq.com>
2023-05-13 22:19:47 +0800

    1. jdk 改为1.8

--- src/main/java/org/yk/cus/helper/DatabaseHelper.java
+++ src/main/java/org/yk/cus/helper/DatabaseHelper.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.yk.cus.util.CollectionUtils;
 import org.yk.cus.util.PropsUtil;
 
-import javax.jws.Oneway;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
